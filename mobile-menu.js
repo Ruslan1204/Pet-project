2456f305b72343d23b08bb9d91fcace15d8d7ae8
Ruslan <113464399+Ruslan1204@users.noreply.github.com>
2023-09-10 20:33:09 +0300

    s

--- mobile-menu.js
+++ mobile-menu.js
@@ -2,26 +2,39 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const mobile = document.querySelector('[data-mobile]');
+  const backdrop = document.querySelector('.js-backdrop__mobile');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
+    mobile.classList.toggle('is-hidden');
 
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
+    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
 
+  function closeMenu(evt) {
+    if (evt.target === evt.currentTarget) {
+      mobile.classList.toggle('is-hidden');
+      backdrop.style.display = 'none';
+    }
+    if (backdrop.style.display === 'none') {
+      backdrop.style.display = '';
+      mobileMenu.classList.remove('is-open');
+    }
+  }
+
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  backdrop.addEventListener('click', closeMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
+    mobile.classList.remove('is-hidden');
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
